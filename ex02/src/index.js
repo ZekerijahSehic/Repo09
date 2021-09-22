a1033ea7db8edaa59a154f1bc877f25e6811b8df
Zekerijah <zekerijahsehic1@gmail.com>
2021-09-22 10:18:33 +0200

    If Statements

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line 
+function myFunction(myParameter) {
+    if(myParameter == true) {
+        console.log("The parameter is true!");
+    } else {
+        console.log("The parameter is false!");
+    }
+}
+
+myFunction(true);
+myFunction(false);
+// Only change code above this line 
+module.exports = myFunction;
