0a742f30dd611dbef4885d022ed2b1d18a2648cd
Zekerijah <zekerijahsehic1@gmail.com>
2021-09-22 10:22:00 +0200

    Comparasion with ||

---
+++ ex08/src/index.js
@@ -0,0 +1,18 @@
+// Only change code below this line 
+function logicalOrOperator(num) {
+    if ( num < 20 || num > 30){
+        console.log("Out");
+    } else {
+        console.log("In");
+    }
+}
+logicalOrOperator(0);
+logicalOrOperator(9);
+logicalOrOperator(20);
+logicalOrOperator(23);
+logicalOrOperator(30);
+logicalOrOperator(31);
+logicalOrOperator(151);
+
+// Only change code above this line 
+module.exports = logicalOrOperator;
