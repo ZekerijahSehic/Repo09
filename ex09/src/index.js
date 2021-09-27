95b1fb2e630aaa56d76f58087bd6792cade9e138
Zekerijah <zekerijahsehic1@gmail.com>
2021-09-27 09:33:21 +0200

    Introducing Else Statements

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,16 +1,16 @@
 // Only change code below this line 
 function functionElse(num) {
     if(num <= 5) {
-        console.log("5 or smaller");
+        return "5 or smaller";
     } else {
-        console.log("Bigger then 5");
+        return "Bigger then 5";
     }
 }
 
-functionElse(4);
-functionElse(5);
-functionElse(6);
-functionElse(10);
+console.log(functionElse(4));
+console.log(functionElse(5));
+console.log(functionElse(6));
+console.log(functionElse(10));
 
 // Only change code above this line 
 module.exports = functionElse;
