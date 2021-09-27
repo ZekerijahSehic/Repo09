25c9aa950540b45a707c834234e095a5eb92b30b
Zekerijah <zekerijahsehic1@gmail.com>
2021-09-27 09:31:38 +0200

    > >= < <=

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,22 +1,22 @@
 // Only change code below this line 
 function comparisonToEqual(a) {
     if (a < 5) {
-        console.log("Less then 5");
+        return "Less then 5";
     }  
     if (a < 10) {
-        console.log("Less then 10");
+        return "Less then 10";
     } 
     if (a >= 10 && a < 20) {
-        console.log("10 or over");
+        return "10 or over";
     }
     if (a > 20) {
-        console.log("More then 20");
+        return "More then 20";
     }
 }
 
-comparisonToEqual(0);
-comparisonToEqual(5);
-comparisonToEqual(17);
-comparisonToEqual(21);
+console.log(comparisonToEqual(0));
+console.log(comparisonToEqual(5));
+console.log(comparisonToEqual(17));
+console.log(comparisonToEqual(21));
 // Only change code above this line 
 module.exports = comparisonToEqual;
