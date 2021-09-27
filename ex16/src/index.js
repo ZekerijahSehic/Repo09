976345f3ca496233603736e8fe49e0b77f65db05
Zekerijah <zekerijahsehic1@gmail.com>
2021-09-27 09:37:58 +0200

    Conditional (Ternary) Operator

--- ex16/src/index.js
+++ ex16/src/index.js
@@ -1,10 +1,10 @@
 // Only change code below this line 
 function checkSign(num) {
-    return (num > 0) ? console.log("positive") : (num < 0) ? console.log("negative") : console.log("zero"); 
+    return (num > 0) ? "positive" : (num < 0) ? "negative" : "zero"; 
 }
 
-checkSign(10);
-checkSign(-12);
-checkSign(0);
+console.log(checkSign(10));
+console.log(checkSign(-12));
+console.log(checkSign(0));
 // Only change code above this line 
 module.exports = checkSign;
