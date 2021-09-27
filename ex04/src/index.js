45511d3770dda22077a900573a1f3f7738f39d67
Zekerijah <zekerijahsehic1@gmail.com>
2021-09-27 09:29:35 +0200

    Comparing different values

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,16 +1,16 @@
 // Only change code below this line 
 function compareDifferentValues(m, n) {
     if (m === n) {
-        console.log("Equal");
+        return "Equal";
     } else {
-        console.log("Not equal");
+        return "Not equal";
     }  
 }
 
-compareDifferentValues(8, "8");
-compareDifferentValues("8", 8);
-compareDifferentValues('8', 8);
-compareDifferentValues("8", "8");
-compareDifferentValues(8, 8);
+console.log(compareDifferentValues(8, "8"));
+console.log(compareDifferentValues("8", 8));
+console.log(compareDifferentValues('8', 8));
+console.log(compareDifferentValues("8", "8"));
+console.log(compareDifferentValues(8, 8));
 // Only change code above this line 
 module.exports = compareDifferentValues;
