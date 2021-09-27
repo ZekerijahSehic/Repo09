776613bc0dd752a88ae5a6be61b92d0d67867229
Zekerijah <zekerijahsehic1@gmail.com>
2021-09-27 09:27:54 +0200

    == ===

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,15 +1,15 @@
 // Only change code below this line 
 function myEqualFunction(num) {
     if (num === 23) {
-        console.log("Equal");
+        return "Equal";
     } else {
-        console.log("Not equal");
+        return "Not equal";
     }
 }
-myEqualFunction(5);
-myEqualFunction(23);
-myEqualFunction("23");
-myEqualFunction('23');
-myEqualFunction("text");
+console.log(myEqualFunction(5));
+console.log(myEqualFunction(23));
+console.log(myEqualFunction("23"));
+console.log(myEqualFunction('23'));
+console.log(myEqualFunction("text"));
 // Only change code above this line 
 module.exports = myEqualFunction;
