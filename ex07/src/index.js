14ecc08d6580031ca9eaf994772fd41bf8b72f88
Zekerijah <zekerijahsehic1@gmail.com>
2021-09-27 09:32:09 +0200

    Comparaion with &&

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,16 +1,16 @@
 // Only change code below this line 
 function logicalAndOperator(num) {
     if ( num <= 80 && num >= 40){
-        console.log("Yes");
+        return "Yes";
     } else {
-        console.log("No");
+        return "No"
     }
 }
-logicalAndOperator(0);
-logicalAndOperator(24);
-logicalAndOperator(45);
-logicalAndOperator(57);
-logicalAndOperator(100);
+console.log(logicalAndOperator(0));
+console.log(logicalAndOperator(24));
+console.log(logicalAndOperator(45));
+console.log(logicalAndOperator(57));
+console.log(logicalAndOperator(100));
 
 // Only change code above this line 
 module.exports = logicalAndOperator;
